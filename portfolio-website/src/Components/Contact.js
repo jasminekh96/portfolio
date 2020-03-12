2677977700c3460cdf48a86835cf44ad833854a9
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2020-03-12 15:10:32 -0400

    fixed colors of icons

--- portfolio-website/src/Components/Contact.js
+++ portfolio-website/src/Components/Contact.js
@@ -9,28 +9,28 @@ const Contact = () => {
 			<header className='contactHeader'>
 				<h4>FULL STACK WEB DEVELOPER</h4>
 				<div className='social'>
-					<div className='item'>
+					<div className='item-contact'>
 						<i className='fas fa-phone-square-alt' />
 						<span>631.245.4439</span>
 					</div>
-					<div className='item'>
+					<div className='item-contact'>
 						<i className='fas fa-map-marker-alt' />
 						<span>Long Island, NY</span>
 					</div>
-					<div className='item'>
+					<div className='item-contact'>
 						<a href='mailto: jasminekh96@gmail.com'>
 							<i className='fas fa-envelope' />
 
 							<span>jasminekh96@gmail.com</span>
 						</a>
 					</div>
-					<div className='item'>
+					<div className='item-contact'>
 						<a href='https://github.com/jasminekh96'>
 							<i className='fab fa-github-square' />
 							<span>github.com/jasminekh96</span>
 						</a>
 					</div>
-					<div className='item'>
+					<div className='item-contact'>
 						<a href='https://www.linkedin.com/in/jasmine-kasheboon-khoury/'>
 							<i className='fab fa-linkedin' />
 							<span>linkedin.com/in/jasmine-kasheboon-khoury</span>
